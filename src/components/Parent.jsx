51cf784849f0c0b56a63415c6ee006dc5665c177
Josh <justinefiesta11@gmail.com>
2026-07-21 03:56:42 +0800

    Flash Cards React App 0.2.0: Functioning Shuffle button

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -7,18 +7,30 @@ import Card from "./Card";
 // hook
 import { useState } from 'react';
 
+function shuffleArray(array) {
+    const arr = [...array]; 
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    
+    }
+  return arr;
+}
+
 const flashcards = card.questions
 
 function Parent(){
     const [index, setIndex] = useState(0);
     
     const [flipCard, setFlipCard] = useState(true)
+
+    const [flashCards, setflashCards] = useState(() => shuffleArray(card.questions))
     
-    let currentCard = flashcards[index]
+    let currentCard = flashCards[index]
     const flip = ()=>{ 
                     setFlipCard(front => !front)
                 }
-
     const previous = ()=>{  
                         (setFlipCard(true))
                         setIndex(i => Math.max(i - 1, 0));  
@@ -26,16 +38,22 @@ function Parent(){
     const next = ()=>{
                         (setFlipCard(true))
                         setIndex(i => Math.min(i + 1, flashcards.length - 1));    
-                    }
+                    }            
+    const shuffle = ()=>{
+                        setflashCards(shuffleArray(card.questions))
+                        setIndex(0)
+    }           
+    
 
 return (
     <>
-        <Statusbar item={index+1} totalItem={flashcards.length} />
+        <Statusbar item={index+1} totalItem={flashCards.length} />
         <Card click={flip} front={currentCard.question} back={currentCard.answer} showAnswer={flipCard}/>
         <div className='btn-container'>
             <Button label={'Previous'} onClick={previous}/>
             <Button label={'Flip'} onClick={flip} />
             <Button label={'Next'} onClick={next}/>
+            <Button label={'Shuffle'} onClick={shuffle}/>
         </div>
     </>
 )
