dcab011f12bf874c12128b63378b5b30afbe0824
Josh <justinefiesta11@gmail.com>
2026-07-21 16:23:08 +0800

    Fixed vite and package

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/Roadmap.sh-Frontend-Project-8---Flash-Cards-/",
   plugins: [react()],
 })
